216a48783b269e6eab71591a4115838107406eff
abeselom-ab <abeselomtesfay@gmail.com>
2025-07-02 22:40:58 +0300

    Add Streamlit web app for facial emotion prediction

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras import models
+import numpy as np
+import os
+import cv2
+model = models.load_models('E:Projects\Python\Facial_Emotion_Recognition\models\Face_Emotion_Classification.keras')
+emotions = [['angry'],
+ ['disgust'],
+ ['fear'],
+ ['happy'],
+ ['neutral'],
+ ['sad'],
+ ['surprise']]
+
+st.header('Human Emotion Recognition')
+image_path = st.text_input('Enter Image Path')
+
+image = cv2.imread(image_path) [:,:,0]
+image = cv2.resize(image, (48,48))
+image = np.invert(np.array([image]))
+
+output = np.argmax(model.predict(image))
+outcome = emotions [output]
+stn = 'Emotion in the Image is '+ str(outcome)
+st.markdown(stn)
+
+image_name = os.path.basename(image_path)
+st.image('Google_Images/'+image_name, width = 300)
